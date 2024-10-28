5bd3b2d1b0c129f8ec29dd4b35d4126117a04fd6
Pobide <pbidet@groupey.fr>
2024-10-28 20:04:50 +0100

    Update gwz_sales.sql

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,3 +1,8 @@
 SELECT  date_date, 
 SUM(turnover) AS turnover FROM `data-analytics-bootcamp-363212.course14.gwz_sales` 
-GROUP BY date_date
+GROUP BY date_date ; 
+
+SELECT date_date,
+SUM(purchase_cost*qty) AS purchase_cost
+FROM `data-analytics-bootcamp-363212.course14.gwz_sales` 
+GROUP BY date_date ; 
